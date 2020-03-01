5695882a96a8837c93f2fa63563f50fdc6c9a30d
Ethan Chen <intervigil@gmail.com>
2020-03-01 00:24:39 -0800

    daisy: Remove bogus ebtables/ethertypes packages
    * These don't exist.
    
    Change-Id: I08f319e26867eb0b8fcdfaa632b4f6061f33b982
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- device.mk
+++ device.mk
@@ -250,11 +250,6 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-impl \
     android.hardware.keymaster@4.0-service
 
-# Ebtables
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes
-
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
