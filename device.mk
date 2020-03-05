63e5cc0eb6d90add79a6a65ae56e63cd31d2db90
TALU <taluemail272@gmail.com>
2020-03-05 22:31:17 +0000

    daisy: Upgrade android.hardware.keymaster to 4.0.
    Change-Id: I62e43a00d63c14537e269c5422ef9aa98c15bb7f
    
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- device.mk
+++ device.mk
@@ -227,6 +227,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
+
 # Ebtables
 PRODUCT_PACKAGES += \
     ebtables \
