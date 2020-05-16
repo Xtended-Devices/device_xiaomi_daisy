daae8978d3312f608e60b45fd8cbb379fc55ef7c
faham1997 <nafidfaham08@gmail.com>
2020-05-17 03:56:15 +0600

    daisy: build vendor.display.config@1.9
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- device.mk
+++ device.mk
@@ -224,7 +224,9 @@ PRODUCT_PACKAGES += \
     libvulkan \
     liboverlay \
     libgenlock \
-    libtinyxml
+    libtinyxml \
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9_vendor
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
