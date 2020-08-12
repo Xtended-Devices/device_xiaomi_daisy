3c15714ed72213a5687aed69583e1540e3ff939b
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    daisy: power: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    [SebaUbuntu]: Cleanup Android.bp and add copyright header
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- device.mk
+++ device.mk
@@ -387,6 +387,10 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx \
     libstagefrighthw
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service
