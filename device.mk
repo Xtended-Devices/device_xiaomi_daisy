8420d2b41f823d2857098aa4b51122d3f75a6871
Adhitya Mohan <me@adhityamohan.in>
2020-09-14 10:43:10 +0530

    daisy: Uprev widevine to 1.3
    - Also add proper context
    
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- device.mk
+++ device.mk
@@ -236,7 +236,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.3-service-lazy.clearkey
+    android.hardware.drm@1.3-service-lazy.clearkey \
+    android.hardware.drm@1.3-service.widevine
 
 # Keymaster
 PRODUCT_PACKAGES += \
