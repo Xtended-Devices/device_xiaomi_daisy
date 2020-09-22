2cff9af11f7dab15d8d523346d07fb3662ba2b12
Arian <arian.kulmer@web.de>
2020-09-22 17:32:07 +0200

    daisy: Add a shim for dpmframework
    * libdpmframework depends on strndup16to8 and strdup8to16 methods.
    
    Change-Id: I39d252b5830b752728a0c8607d32d8a279524ce1
    Co-authored-by: Demon000 <demonsingur@gmail.com>
    
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- device.mk
+++ device.mk
@@ -221,6 +221,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# DPM
+PRODUCT_PACKAGES += \
+    libshim_dpmframework
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
