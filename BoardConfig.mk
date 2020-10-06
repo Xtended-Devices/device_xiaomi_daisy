18c6ca7dc824fafb38146932f25662d52b3d9032
TogoFire <italomellopereira@gmail.com>
2020-10-06 19:03:16 -0300

    daisy: Build kernel using proton clang version
    * git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton
    
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,6 +161,10 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/daisy
 TARGET_KERNEL_CONFIG := sleepy_defconfig
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_COMPILE := true
+# Clang Version
+TARGET_KERNEL_CLANG_VERSION := proton
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
