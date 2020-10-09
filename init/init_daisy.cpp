2c0e27d395a907b4a32e14de4a4d187a225ca19c
Park Ju Hyung <qkrwngud825@gmail.com>
2020-10-09 09:25:39 +0300

    daisy: init: workaround SafetyNet check
    Doing this in the userspace allows more properties to be spoofed
    and eliminate the needs for a hack in the kernel.
    
    Change-Id: Id094413b9fc5b9763434183448bdd5748397341f
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- init/init_daisy.cpp
+++ init/init_daisy.cpp
@@ -37,6 +37,8 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -53,6 +55,39 @@ char const *heapsize;
 char const *heapminfree;
 char const *heapmaxfree;
 char const *heaptargetutilization;
+
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+  "ro.boot.vbmeta.device_state",
+  "ro.boot.verifiedbootstate",
+  "ro.boot.flash.locked",
+  "ro.boot.selinux",
+  "ro.boot.veritymode",
+  "ro.boot.warranty_bit",
+  "ro.warranty_bit",
+  "ro.debuggable",
+  "ro.secure",
+  "ro.build.type",
+  "ro.build.tags",
+  "ro.build.selinux",
+  NULL
+};
+
+static const char *snet_prop_value[] = {
+  "locked",
+  "green",
+  "1",
+  "enforcing",
+  "enforcing",
+  "0",
+  "0",
+  "0",
+  "1",
+  "user",
+  "release-keys",
+  "1",
+  NULL
+};
 
 void property_override(char const prop[], char const value[])
 {
@@ -107,6 +142,17 @@ void check_device()
     }
 }
 
+static void workaround_snet_properties() {
+
+  // Hide all sensitive props
+  for (int i = 0; snet_prop_key[i]; ++i) {
+    property_override(snet_prop_key[i], snet_prop_value[i]);
+  }
+
+  chmod("/sys/fs/selinux/enforce", 0640);
+  chmod("/sys/fs/selinux/policy", 0440);
+}
+
 void vendor_load_properties()
 {
     check_device();
@@ -123,5 +169,8 @@ void vendor_load_properties()
     property_override("ro.product.device", "daisy");
 
     property_override("ro.oem_unlock_supported", "0");
+
+    // Workaround SafetyNet
+    workaround_snet_properties();
     
 }
