3249adb9b39765685c5c61b848379a363b81eb87
tangrobin <tangrobin@google.com>
2020-10-18 20:38:17 +0000

    daisy: use phase offsets as duration
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Adopt the new way of specifying
    phase offsets to SurfaceFlinger.
    
    Bug: 158464992
    Test: scrolling, app launching/closing, playing video
    
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- prop.mk
+++ prop.mk
@@ -95,6 +95,7 @@ debug.sf.hw=0 \
 debug.sdm.support_writeback=0 \
 debug.sf.disable_backpressure=1 \
 debug.sf.enable_gl_backpressure=1 \
+debug.sf.enable_hwc_vds=1 \
 debug.sf.latch_unsignaled=0 \
 debug.cpurend.vsync=false \
 debug.sf.recomputecrop=0 \
@@ -236,15 +237,18 @@ persist.vendor.cne.logging.qxdm=3974
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.protected_contents=true \
-ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.max_virtual_display_dimension=4096
 
-PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.early_phase_offset_ns=1500000 \
-debug.sf.early_app_phase_offset_ns=1500000 \
-debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000
+# Phase offsets
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+debug.sf.use_phase_offsets_as_durations=1 \
+debug.sf.late.sf.duration=10500000 \
+debug.sf.late.app.duration=20500000 \
+debug.sf.early.sf.duration=21000000 \
+debug.sf.early.app.duration=16500000 \
+debug.sf.earlyGl.sf.duration=13500000 \
+debug.sf.earlyGl.app.duration=21000000
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
