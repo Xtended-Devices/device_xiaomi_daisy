835771c54c58db159d23868f1093378086da4b29
Vishalcj17 <vishalcj@aospa.co>
2020-11-12 12:25:56 +0100

    daisy: switch to File-Based Encryption
    Moar security!
    Will probably force AOSP recovery too, yay!
    
    Change-Id: I0f07dfce010a81d7e7550b941b0988c40c6be35f
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- device.mk
+++ device.mk
@@ -282,10 +282,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
