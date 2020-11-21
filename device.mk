38bad654fe2df41837fbf74ede9e5090ba5e6253
SakilMondal <mondalsakil@gmail.com>
2020-11-21 22:51:50 +0100

    daisy: Build QTI thermal 2.0 hal
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- device.mk
+++ device.mk
@@ -450,6 +450,9 @@ PRODUCT_HOST_PACKAGES += \
     signapk
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
