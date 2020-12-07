38bf033c4e6179f60d249ff94a4f6a12468573e1
Lalit Maganti <lalitm@google.com>
2020-12-07 10:08:37 +0000

    daisy: Disable perfetto
    Change-Id: Ic2996ca903bc6f7f203cb8b2976e04bed5a71289
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- prop.mk
+++ prop.mk
@@ -191,6 +191,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.sys.fw.dex2oat_thread_count=8 \
 ro.vendor.extension_library=libqti-perfd-client.so
 
+# Perfetto
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.traced.enable=0
+
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.vendor.use_data_netmgrd=true \
