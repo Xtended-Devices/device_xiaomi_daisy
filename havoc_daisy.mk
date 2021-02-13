21dcd31980bc9a29b31063484c8654d6262f3da0
TogoFire <italomellopereira@gmail.com>
2021-02-12 22:09:36 -0300

    daisy: Update to Sunfish February 2021 build fingerprint
    Change-Id: af3b6a1e3b5926a9d5f8d5667ea6fc47ba0e6940
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- havoc_daisy.mk
+++ havoc_daisy.mk
@@ -48,10 +48,10 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sunfish-user 11 RP1A.201105.002 6869500 release-keys"
+    PRIVATE_BUILD_DESC="sunfish-user 11 RQ1A.210205.004 7038034 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RP1A.201105.002/6869500:user/release-keys"
+BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RP1A.210205.004/7038034:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
