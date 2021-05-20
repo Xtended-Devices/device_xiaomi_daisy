3b302d4d2a300f7fa47701394002e195bb41e987
Georg Veichtlbauer <georg@vware.at>
2021-05-20 07:18:46 +0200

    daisy: Remove non-existant SEPolicy dirs from BoardConfig
    Change-Id: Iff1f66676ef11612b8ace86fe17a2f015ce4b59c
    
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,8 +206,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # SurfaceFlinger
