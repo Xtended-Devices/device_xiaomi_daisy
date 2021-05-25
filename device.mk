fbb64a73d153bf1cd01371f9446f27aa5334a464
henricaodopao <allricknation@gmail.com>
2021-05-25 19:50:48 +0000

    daisy: dont build QTI power hal
    It doesn't work
    
    * E QTI PowerHAL: Failed to perform launch boost
    * E QTI PowerHAL: Failed to acquire lock.
    
    Link: https://github.com/Berkeclb/android_device_asus_X00QD-berke/commit/d4522a637203545c01057f272fca030df82da6fd
    Change-Id: 8786d25025bd315b43c377d6ff43a10d4389b527
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- device.mk
+++ device.mk
@@ -417,7 +417,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor
 
