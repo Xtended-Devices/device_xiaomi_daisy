2fd09b5ec97c40ecc838dc02665e2d747ed43f1b
Pascoato <viniciuspascoato@gmail.com>
2021-06-09 23:34:45 -0300

    daisy: Drop libhidl & build vendor variant
    build/make/core/base_rules.mk:342: error: android_device_xiaomi_daisy/camera/mm-image-codec/qomx_core: MODULE.TARGET.SHARED_LIBRARIES.android.hidl.base@1.0 already defined by hardware/lineage/compat.
    
    E vndksupport: Could not load /vendor/lib64/hw/fingerprint.fpc.default.so from default namespace: dlopen failed: library "android.hidl.base@1.0.so" not found: needed by /vendor/lib64/com.fingerprints.extension@1.0.so in namespace (default).
    E HAL     : load: module=/vendor/lib64/hw/fingerprint.fpc.default.so
    
    This also helps not to run out of fingerprint.
    
    Change-Id: 54126be4be2d46cd35a21505e2bb26dba016c836
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- device.mk
+++ device.mk
@@ -294,8 +294,11 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
+    android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor
+
+PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder \
     libhidltransport.vendor \
