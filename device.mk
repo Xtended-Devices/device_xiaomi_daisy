bf709b9fed0eaf6e5741def9ba18b7de4ce2292c
TogoFire <italomellopereira@gmail.com>
2021-08-05 08:39:08 -0300

    daisy: downrev keymaster to 3.0
    This reverts commit 63e5cc0eb6d90add79a6a65ae56e63cd31d2db90.
    
    * We use keymaster 3.0 for FBE
    
    Change-Id: 4a9a17095256732afe27af3dda46b9fe70810ff5
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- device.mk
+++ device.mk
@@ -237,11 +237,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service-lazy.clearkey \
     android.hardware.drm@1.3-service.widevine
 
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0-impl \
-    android.hardware.keymaster@4.0-service
-
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
