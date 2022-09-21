3f8f88c67a4cb4ed79c7c36887f31cd2fbeb46ce
TogoFire <italomellopereira@gmail.com>
2022-09-21 10:32:39 -0300

    daisy: parts: drop hal switcher
    This reverts commits 4f7020a113b11bdee3fdfe8c303b7c3628c0510a, f04ee898c2ce8e7951ad03061d5a4130698c1396, 9c22a1490cd9ff65bb5a11595fde63ef2c42d3f4
    
    Change-Id: ed37ce7e94adce09b2a8db916323b41436c3747e
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- device.mk
+++ device.mk
@@ -431,7 +431,6 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.camera.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
@@ -441,7 +440,6 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
-    init.camera.sh \
     init.class_main.sh \
     init.goodix.sh \
     init.qcom.early_boot.sh \
