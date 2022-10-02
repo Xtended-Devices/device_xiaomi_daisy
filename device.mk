f71bac4c5bb71a2a34b5ac26538cd95797c26b20
Bruno Martins <bgcngm@gmail.com>
2022-10-02 23:50:32 +0200

    daisy: Drop component overrides
    This is no longer needed as of Android 13.
    
    Change-Id: b34ef1760baa97264fb14b7470c5d0b3386b2c7d
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- device.mk
+++ device.mk
@@ -207,10 +207,6 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libmm-qcamera
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
