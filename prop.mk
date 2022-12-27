3cb0ddd6cedb12a04cb7ef0de45ef2f2f28ed22a
baunilla <baunilla@users.noreply.github.com>
2022-12-27 08:25:11 +0530

    daisy: props: Update supported qcom parsers
    * From FP3:11/8901.4.A.0016.6/gms-f0c20986:user/release-keys
    
    Change-Id: 00f14ce962e9e73f60a494724df5e9cd60a2f39a
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- prop.mk
+++ prop.mk
@@ -173,7 +173,6 @@ debug.stagefright.omx_default_rank=0 \
 media.msm8956hw=0 \
 media.stagefright.audio.sink=280 \
 media.stagefright.thumbnail.prefer_hw_codecs=true \
-vendor.mm.enable.qcom_parser=1048575 \
 mm.enable.smoothstreaming=true \
 mm.enable.sec.smoothstreaming=true \
 mmp.enable.3g2=true \
@@ -198,6 +197,10 @@ ro.vendor.qti.sys.fw.bservice_enable=true
 PRODUCT_SYSTEM_PROPERTIES += \
 persist.sys.fuse.transcode_user_control=true \
 persist.sys.fuse.transcode_enabled=false
+
+# Codecs:(PARSER_)AAC AC3 AMR_NB AMR_WB ASF DTS FLV 3GP 3G2 MKV MP2PS MP2TS MP3 OGG QCP WAV FLAC AIFF APE
+PRODUCT_VENDOR_PROPERTIES += \
+vendor.mm.enable.qcom_parser=1048543
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
